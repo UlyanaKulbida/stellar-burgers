2ca4cd31afa4b59ebc1caf1a6a379fa4257bf621
Ulyana Kulbida <unikub@yandex.ru>
2024-12-24 13:05:01 +0600

    test(ingredientslicetest):  fix initialState

--- src/services/slices/__tests__/ingredientSlice.test.ts
+++ src/services/slices/__tests__/ingredientSlice.test.ts
@@ -2,18 +2,18 @@ import {
   getIngredientsList,
   ingredientsSliceReducer
 } from '../ingredientSlice';
+import { initialState as rootInitialState } from './rootReducer.test';
+import { INGREDIENT_ERROR_MESSAGE } from '../../../utils/constants.utils';
 
 describe('Ingredients reducer', () => {
-  const initialState = {
-    ingredients: [],
-    loading: false,
-    error: null
-  };
+  const initialState = rootInitialState.burgerIngredients;
+  const PENDING_TYPE = getIngredientsList.pending.type;
+  const FULFILLED_TYPE = getIngredientsList.fulfilled.type;
+  const REJECTED_TYPE = getIngredientsList.rejected.type;
 
   it('обрабатка состояния ожидания', () => {
-    const action = { type: getIngredientsList.pending.type };
+    const action = { type: PENDING_TYPE };
     const newState = ingredientsSliceReducer(initialState, action);
-
     expect(newState.loading).toBe(true);
     expect(newState.error).toBeNull();
   });
@@ -21,25 +21,22 @@ describe('Ingredients reducer', () => {
   it('оработка успешной нагрузки', () => {
     const ingredients = [{ _id: '1', name: 'Test' }];
     const action = {
-      type: getIngredientsList.fulfilled.type,
+      type: FULFILLED_TYPE,
       payload: ingredients
     };
     const newState = ingredientsSliceReducer(initialState, action);
-
     expect(newState.loading).toBe(false);
     expect(newState.ingredients).toEqual(ingredients);
     expect(newState.error).toBeNull();
   });
 
   it('оработка сбойной нагрузки', () => {
-    const error = 'Не удалось загрузить ингредиенты';
     const action = {
-      type: getIngredientsList.rejected.type,
-      error: { message: error }
+      type: REJECTED_TYPE,
+      error: { message: INGREDIENT_ERROR_MESSAGE }
     };
     const newState = ingredientsSliceReducer(initialState, action);
-
     expect(newState.loading).toBe(false);
-    expect(newState.error).toBe(error);
+    expect(newState.error).toBe(INGREDIENT_ERROR_MESSAGE);
   });
 });
