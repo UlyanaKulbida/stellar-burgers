86fc46a8ba07797fc0cec8105d6fc033054e4f68
Ulyana Kulbida <unikub@yandex.ru>
2024-12-24 15:10:17 +0600

    test(e2e):  добавлены commands

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,17 +1,14 @@
-// ***********************************************************
-// This example support/e2e.ts is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+import './commands';
 
-// Import commands.js using ES2015 syntax:
-import './commands'
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      addBunToConstructor(): Chainable<void>;
+      addIngredientToConstructor(type: string, name: string): Chainable<void>;
+      openIngredientModal(ingredientName: string): Chainable<void>;
+      closeModal(): Chainable<void>;
+      closeModalByOverlay(): Chainable<void>;
+      createOrder(): Chainable<void>;
+    }
+  }
+}
